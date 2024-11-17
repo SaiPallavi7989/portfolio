8092b890608b54d67cabc45982fa9d628f40cc8c
Pallavi Vanjarapu <161013366+SaiPallavi7989@users.noreply.github.com>
2024-11-18 00:12:06 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 
 
 // element toggle function
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); } 
 
 
 
